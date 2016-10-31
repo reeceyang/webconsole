42ce2ae2519123c198cd793453357e858d8e5d4c
Reece Yang <reeceyang@live.com>
2016-10-31 10:15:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var line;
+
 function init() {
   var input = document.getElementById("in");
   var out = document.getElementById("out");
   input.addEventListener("input", function() {
-    out.innerHTML = input.innerHTML;
+    line = input.innerHTML;
+    output.innerHTML = line;
   });
 }
