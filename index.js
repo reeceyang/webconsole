038af6a691c1e23c47d94b5806e9069d257b56db
Reece Yang <reeceyang@live.com>
2016-11-05 14:47:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function init() {
   var line;
+  var oldLength;
   var input = document.getElementById("in");
   var out = document.getElementById("out");
   input.addEventListener("input", function() {
-    console.log("event hit")
     line = input.innerHTML;
-    out.innerHTML = line;
+    out.innerHTML = out.innerHTML.substring(0, out.innerHTML.length()-oldLength);
+    out.innerHTML += line;
+    oldLength = line.length();
   });
 }
