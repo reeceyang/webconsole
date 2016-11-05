a9e31652f0c6388ceb0ba0df5138cafb00948b8a
Reece Yang <reeceyang@live.com>
2016-11-05 15:22:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,16 @@ function init() {
     out.innerHTML += stuff;
   }
   println("Hello World");
+  
+  input.addEventListener("onkeypress", function() {
+    var keynum;
+
+    if(window.event) { // IE                    
+      keynum = e.keyCode;
+    } else if(e.which){ // Netscape/Firefox/Opera                   
+      keynum = e.which;
+    }
+
+    alert(String.fromCharCode(keynum));
+  });
 }
