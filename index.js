27d59a3284ebcea38bff104ff39cae0a19250e27
Reece Yang <reeceyang@live.com>
2016-11-05 14:53:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function init() {
   var out = document.getElementById("out");
   input.addEventListener("input", function() {
     line = input.innerHTML;
-    out.innerHTML = out.innerHTML.substring(0, (out.innerHTML).length()-oldLength);
+    out.innerHTML = out.innerHTML.substring(0, out.innerHTML.length-oldLength);
     out.innerHTML += line;
-    oldLength = line.length();
+    oldLength = line.length;
   });
 }
