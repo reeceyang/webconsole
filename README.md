93537deaee8dffd8383c3eb1616c069ccb42b5bb
reeceyang <reeceyang@live.com>
2020-11-26 19:33:16 -0600

    fix everything up

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # webconsole
 Provides a web console HTML page with print and input functions for JavaScript.
+
+## Usage
+1. Copy index.css, index.html, and index.js into your project.
+
+2. Write your code in an asynchronous main function:
+```javascript
+async function main() {
+  // do stuff
+}
+```
+
+3. Call the main function to run your program:
+
+```javascript
+main();
+```
+
+## Functions
+
+#### `print(arg1, arg2, ...)`
+
+Prints all arguments to the console, one after another.
+
+#### `println(arg1, arg2, ...)`
+
+Prints all arguments to the console, on after another, with a line break at the end.
+
+#### `await input(arg1, arg2, ...)`
+
+Prints all arguments to the console, then waits for the user to input a value. Then prints a line break at the end, and returns that value. *Note:* you must place a `await` in front because `input()` is an asynchronous function.
--- index.js
+++ index.js
@@ -1,60 +1,43 @@
-// function init() {
-//   var line;
-//   var oldLength = 0;
-//   var input = document.getElementById("in");
-//   var out = document.getElementById("out");
-//   input.addEventListener("input", function() {
-//     line = input.innerHTML;
-//     out.innerHTML = out.innerHTML.substring(0, out.innerHTML.length-oldLength);
-//     out.innerHTML += line;
-//     oldLength = line.length;
-//   });
-//   function println(stuff) {
-//     out.innerHTML += stuff + "<br>";
-//   }
-//   function print(stuff) {
-//     out.innerHTML += stuff;
-//   }
-//   println("Hello World");
-// }
-// function println(stuff, out) {
-//   out.innerHTML += stuff + "<br>";
-// }
-// function print(stuff, out) {
-//   out.innerHTML += stuff;
-// }
+// from https://stackoverflow.com/questions/2794137/sanitizing-user-input-before-adding-it-to-the-dom-in-javascript
+function sanitize(string) {
+  const map = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#x27;',
+      "/": '&#x2F;',
+      "`": '&grave;'
+  };
+  const reg = /[&<>"'/]/ig;
+  return string.replace(reg, (match)=>(map[match]));
+}
 
 var inp = document.getElementById("in");
 var out = document.getElementById("out");
 
 var inputting = false;
 
-// should probably update these to take no arguments
-function println(stuff) {
-  out.innerHTML += stuff + "<br>";
+function println() {
+  for (i in arguments) {
+    print(arguments[i]);
+  }
+  print("<br>");
 }
 function print(stuff) {
   out.innerHTML += stuff;
+  out.scrollTop += 100;
 }
 
-// var enterPressed = new Promise(function(resolve, reject) {
-//   inp.addEventListener("keyup", function(event) {
-//     if(event.key === "Enter" & inputting) {
-//       //inp.innerHTML = inp.innerHTML.substring(0, out.innerHTML.length - 1);
-//       //enterPressed = true;
-//       console.log("enter pressed");
-//       resolve("Yes");
-//     }
-//   });
-// });
-
 async function input() {
+  for (i in arguments) {
+    print(arguments[i]);
+  }
+
   var enterPressed = new Promise(function(resolve, reject) {
     inp.addEventListener("keyup", function(event) {
       if(event.key === "Enter" & inputting) {
-        //inp.innerHTML = inp.innerHTML.substring(0, out.innerHTML.length - 1);
-        //enterPressed = true;
-        console.log("enter pressed");
+        //console.log("enter pressed");
         resolve("Yes");
       }
     });
@@ -62,12 +45,10 @@ async function input() {
 
   inputting = true;
   await enterPressed;
-  var stuff = inp.value;
+  var stuff = sanitize(inp.value);
   inp.value = "";
   print(stuff);
-  println("");
-  enterPressed = Promise.reject("inputted");
-  console.log(stuff);
+  println();
   inputting = false;
   return stuff;
 }
@@ -76,8 +57,7 @@ async function main() {
   var num = Math.floor((Math.random() * 100) + 1);
   guess = -1;
   while (guess != num) {
-    println("guess a number between 1 and 100 ");
-    var guess = parseInt(await input());
+    var guess = parseInt(await input("guess a number between 1 and 100: "));
     if (guess > num) {
       println("too high");
     } else if (guess < num) {
